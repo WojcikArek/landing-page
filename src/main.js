9751b0ff30d7e1b7bf1de7ea11d08e5365fb295d
Arek <wojcikarek10@gmail.com>
2023-01-17 21:06:29 +0100

    add font awsome core

--- src/main.js
+++ src/main.js
@@ -3,12 +3,19 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faUserSecret)
 
 import "./assets/main.css";
 
 const app = createApp(App);
 
-app.use(createPinia());
-app.use(router);
+createApp(App)
+.use(createPinia())
+.use(router)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
 
-app.mount("#app");
