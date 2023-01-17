9c874d1d31d11f5626e1843a366c14d8f4d06393
Arek <wojcikarek10@gmail.com>
2023-01-17 21:06:08 +0100

    add footer, nav, form

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import NavBar from './components/NavBar.vue'
+import NavBar from './components/NavBar.vue';
+import FormValid from "./components/FormValid.vue";
+import Footer from "./components/Footer.vue"
 </script>
 
 <template>
  <NavBar/>
  <RouterView/>
+ <FormValid/>
+ <Footer/>
 </template>
 
 <style scoped>
